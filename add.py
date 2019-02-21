797e9f1e10e0e93890521580d544b37d339e2e59
”André <andre.heydran@gmail.com>
2019-02-21 09:07:34 -0300

    msg

--- add.py
+++ add.py
@@ -1,6 +1,11 @@
 import os
 msg = input("Qual a mensagem de atualização ")
+os.system("git config --global user.name ”André Feuzer”")
+os.system("git config --global user.email andre.heydran@gmail.com")
 os.system("git add *")
 m ="git commit -m " + "\'" + " msg" + "\'"
 os.system(m)
 os.system("git push")
+
+os.system("git config --global user.name ”FULADO”")
+os.system("git config --global user.email FULAON.CICLANO@mail.com")
