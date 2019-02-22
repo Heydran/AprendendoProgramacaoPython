b307bb0283f8e923080dc319739a88dc39a87858
”André <andre.heydran@gmail.com>
2019-02-22 20:55:04 -0300

    commit de casa teste

--- add.py
+++ add.py
@@ -3,6 +3,5 @@ msg = input("Qual a mensagem de atualização ")
 os.system("git config --global user.name ”André Feuzer”")
 os.system("git config --global user.email andre.heydran@gmail.com")
 os.system("git add *")
-m ="git commit -m " + "\'" + " msg" + "\'"
-os.system(m)
+m ="git commit -m '", msg, "'"
 os.system("git push")
