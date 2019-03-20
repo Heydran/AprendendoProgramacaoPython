5c7665185c1ab8f98e92802e9ec0124929d06578
”André <andre.heydran@gmail.com>
2019-03-20 13:46:54 -0300

    css

--- servidor.py
+++ servidor.py
@@ -1,9 +1,9 @@
 class Pessoa(object):
-	def __init__(self, nome, idade, sexo):
+	def __init__(self, nome,  telefone, endereco):
 		self.nome = nome
-		self.idade = idade
-		self.sexo = sexo
-lista = [Pessoa("André","0","INDEFINIDO"),Pessoa("Heydran","Infinita","TODOS")]
+		self.telefone = telefone
+		self.endereco = endereco
+lista = [Pessoa("Lester","(560) 483-0293","São Paulo"),Pessoa("Solomon","(852) 535-1460","Quebec"),Pessoa("Kane","(342) 663-1815","Västra Götalands län"),Pessoa("Jeanette","(491) 924-6893","Provence-Alpes-Côte d'Azur"),Pessoa("Herman","(234) 414-9654","Île-de-France"),Pessoa("Joseph","(274) 796-9678","Oyo"),Pessoa("Blythe","(633) 709-8290","Västra Götalands län"),Pessoa("Bradley","(258) 860-2514","West-Vlaanderen"),Pessoa("Ashton","(355) 140-9940","LOM"),Pessoa("Russell","(999) 750-6924","Zuid Holland")]
 
 
 
@@ -16,3 +16,6 @@ def inicio():
 def listar_pessoas():
 	return render_template("listar_pessoas.html", users = lista)
 app.run()
+
+
+
