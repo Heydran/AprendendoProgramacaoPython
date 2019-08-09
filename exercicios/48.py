2c98ef4ebbdc0192c5501cea022f44c77d30978a
”André <andre.heydran@gmail.com>
2019-08-09 10:29:48 -0300

    exercicio

--- exercicios/48.py
+++ exercicios/48.py
@@ -6,3 +6,43 @@ espécie deve ser podada a cada dois meses, enquanto outra espécie precisa ser 
 Repita os itens b) e c) da questão anterior, com as novas informações inseridas nesta questão."
 
 
+from peewee import *
+from os import system
+
+db = SqliteDatabase("Planta.db")
+
+class Modelo(Model):
+	class Meta:
+		database = db
+
+
+class Jardim(Modelo):
+	nome = CharField()
+
+
+class Planta(Modelo):
+	nome = CharField()
+	nome_cientifico = CharField()
+	tamanho_da_folha = CharField()
+	periodo_poda = IntegerField()
+
+class Planta_do_Jardim(Modelo):
+	jardim = ForeignKeyField(Jardim)
+	planta = ForeignKeyField(Planta)
+	data_plantada = CharField()
+
+if __name__ == '__main__':
+	system("rm -f Planta.db")
+	db.connect()
+	db.create_tables([Jardim, Planta, Planta_do_Jardim])
+	Planta(nome = "orquidea", nome_cientifico = "orquedeos", tamanho_da_folha = "infinito", jardim = 1, periodo_poda = 30).save()
+	q1 = Planta.select()
+	for i in q1:
+		print(i.nome, i.nome_cientifico, i.tamanho_da_folha)
+	Jardim(nome = "label_name_jardin").save()
+
+	Planta_do_Jardim(jardim = 1, planta = 1,data_plantada = "12/05/2003").save()
+	
+	q2 = Planta_do_Jardim.select().where(Planta_do_Jardim.planta == Planta.get_by_id(1))
+	
+	print(q2[0].data_plantada)
--- exercicios/49.py
+++ exercicios/49.py
@@ -10,3 +10,42 @@ Faça as seguintes tarefas:\
 requisições do laboratório.\
 (b) Crie classes (com uso do peewee) que permitam armazenar as informações das requisições.\
 (c) Faça o teste das classes."
+
+from peewee import *
+from os import system
+
+#get_through_model
+
+db = SqliteDatabase("49.db")
+
+class Modelo(Model):
+	class Meta():
+		database = db
+
+class Pessoa(Modelo):
+	nome = CharField()
+
+class Medico(Pessoa):
+	pass
+
+class Paciente(Pessoa):
+	medico = ManyToManyField(Medico)
+
+'''
+class Exame(Modelo):
+	paciente = ForeignKeyField(Paciente)
+	medico = ForeignKeyField(Medico)
+	data_requisicao = DateField()#9999-99-99
+	tipo_exame = CharField() '''
+
+if __name__ == '__main__':
+	system("rm -f 49.db")
+	db.connect()
+	db.create_tables([Paciente.medico.get_through_model(), Medico])
+	m = Medico(nome = "Beltrano").save()
+	Paciente(nome = "Fulano", medico = m ).save()
+	
+	#Exame(paciente = 1, medico = 1,data_requisicao = "9999-99-99",tipo_exame = "raio x").save()
+	#q1 = Exame_Requisitado.select().where(Exame_Requisitado.paciente == Paciente.get_by_id(1))
+	#print(q1[0].paciente.nome)
+
